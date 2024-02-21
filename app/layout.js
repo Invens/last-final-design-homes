5f7ec934d58ae6f507ed80160359c67c8b8b5b30
Abhishek kumar <41527391+Invens@users.noreply.github.com>
2024-02-21 06:09:56 +0000

    id

--- app/layout.js
+++ app/layout.js
@@ -17,15 +17,17 @@ const bioRhyme = BioRhyme({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <Script strategy='lazyOnLoad'>{`
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3553114,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+     <Script strategy='lazyOnLoad'>
+        {`
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3553114,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
       </Script>
       <body className={bioRhyme.className}>
       
